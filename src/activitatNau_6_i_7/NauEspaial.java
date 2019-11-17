96388ac153ae15a0e1810a9c36e7fc1a17ffeae0
juan <juanxue6@gmail.com>
2019-11-17 18:13:11 +0100

    Actualizacion

---
+++ src/activitatNau_6_i_7/NauEspaial.java
@@ -0,0 +1,289 @@
+package activitatNau_6_i_7;
+
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class NauEspaial extends javax.swing.JFrame {    
+    
+    public NauEspaial() {
+        initComponents();
+        }
+
+    @SuppressWarnings("unchecked")
+    private void initComponents() {
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(255, 255, 255));
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGap(0, 400, Short.MAX_VALUE));
+        layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGap(0, 300, Short.MAX_VALUE));
+        pack();
+        }
+    
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                    }
+                }
+            } 
+        catch (Exception ex) {
+            java.util.logging.Logger.getLogger(NauEspaial.class.getName()).log(
+                java.util.logging.Level.SEVERE, null, ex);
+            }       
+        NauEspaial f = new NauEspaial();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setTitle("Naus Espaials");
+        f.setContentPane(new PanelNau());
+        f.setSize(480, 560);
+        f.setVisible(true);
+        }
+    }
+
+
+class PanelNau extends JPanel implements Runnable, KeyListener{   
+    private int numNaus=3;    
+    Nau[] nau;
+    Shot[] shot = new Shot[10];
+    private int contador = 0;
+    
+    int a = shot.length;
+  
+
+    Nau nauPropia;
+    Shot Actualshot;
+
+    public PanelNau(){        
+        nau = new Nau[numNaus];
+        for (int i=0;i<nau.length;i++) {
+            Random rand = new Random();
+            int velocitat=(rand.nextInt(3)+5)*10;
+            int posX=rand.nextInt(100)+30;
+            int posY=rand.nextInt(100)+30;
+            int dX=rand.nextInt(3)+1;
+            int dY=rand.nextInt(3)+1;
+            String nomNau = Integer.toString(i);
+            nau[i]= new Nau(nomNau,posX,posY,dX,dY,velocitat);
+            }
+        
+        // Creo la nau propia
+        nauPropia = new Nau("NauNostra",200,400,10,0,100);
+        
+        
+        // Creo fil per anar pintant cada 0,1 segons el joc per pantalla
+        Thread n = new Thread(this);
+        n.start();         
+        
+        // Creo listeners per a que el fil principal del programa gestioni
+        // esdeveniments del teclat
+        addKeyListener(this);
+        setFocusable(true);               
+        
+        }
+
+    public void run() {
+        System.out.println("Inici fil repintar");
+        while(true) {
+            try { Thread.sleep(100);} catch(Exception e) {} // espero 0,1 segons
+            //System.out.println("Repintant");
+            repaint(); 
+            }                   
+        }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for(int i=0; i<nau.length;++i) nau[i].pinta(g);
+        nauPropia.pinta(g);
+        
+        for (int i = 0; i < shot.length; i++) {
+            if(shot[i] != null) {
+            	shot[i].printShot(g);
+ x
+            }
+        	
+		}
+
+        
+     }
+    
+ public void newShot() throws InterruptedException {
+
+	 if(contador < 9) {
+	 shot[contador]=  new Shot(nauPropia.getX() + 69, nauPropia.getY() - 40 , 50); 
+	 contador++;
+
+	 }else {
+		// Thread.sleep(5000);
+		 int coordenada = shot[8].getY();
+		 for (int i = 0; i < shot.length; i++) {
+			shot[i] = null;
+		}
+		 contador = 0;
+		 
+	 }	 
+ }
+ 
+ 
+
+     
+        // Metodes necesaris per gestionar esdeveniments del teclat
+        @Override
+        public void keyTyped(KeyEvent e) {
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            //System.out.println("Key pressed code=" + e.getKeyCode() + ", char=" + e.getKeyChar());
+            if (e.getKeyCode()==37) { nauPropia.esquerra();} //System.out.println("a l'esquerra"); }
+            if (e.getKeyCode()==39) { nauPropia.dreta(); }
+            if (e.getKeyCode()==32) { try {
+				newShot();
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}}//System.out.println("a la dreta"); }  
+            
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+        	 if (e.getKeyCode()==37) { nauPropia.parar();} //System.out.println("a l'esquerra"); }
+             if (e.getKeyCode()==39) { nauPropia.parar(); }
+        }
+}
+   
+class Shot extends Thread{
+	
+
+    private int dsx,dsy,v;
+    private int tx = 10;
+    private int ty = 10;
+
+
+
+    private Image laser;
+
+    public Shot (int dsx, int dsy, int v ) {
+        this.dsy=dsy;
+        this.dsx=dsx;
+        this.v=v;
+        laser = new ImageIcon(Nau.class.getResource("laser.png")).getImage();
+        Thread t = new Thread(this); 
+        t.start();
+        }
+   
+    
+    public int velocitat (){ 
+        return v;
+        }
+    
+    
+    public void printShot (Graphics g) {
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.drawImage(this.laser, this.dsx, this.dsy, null);
+      }
+    
+    public void moure (){
+    	int dsy = 25;
+    		this.dsy = this.dsy - dsy;
+        }
+    
+    public void run() {
+        while (true) { 
+            try { Thread.sleep(this.v); } catch (Exception e) {}
+            moure();
+            
+            }
+        }
+    
+    public int getY() {
+    	return this.dsy;
+    }
+    
+    public int getX() {
+    	return this.dsx;
+    }
+    
+   
+
+}
+
+        
+class Nau extends Thread {
+    private String nomNau;
+    private int x,y;
+    private int dsx,dsy,v;
+    private int tx = 10;
+    private int ty = 10;
+
+
+    private String img = "/images/nau.jpg";
+    private Image image;
+    private Image laser;
+
+    public Nau(String nomNau, int x, int y, int dsx, int dsy, int v ) {
+        this.nomNau = nomNau;
+        this.x=x;
+        this.y=y;
+        this.dsx=dsx;
+        this.dsy=dsy;
+        this.v=v;
+
+        
+        image = new ImageIcon(Nau.class.getResource("nau.png")).getImage();
+        laser = new ImageIcon(Nau.class.getResource("laser.png")).getImage();
+        Thread t = new Thread(this); 
+        t.start();
+        }
+    
+    public int velocitat (){ 
+        return v;
+        }
+    
+    public void moure (){
+        x=x + dsx;
+        y=y + dsy;
+        // si arriva als marges ...
+        if ( x>= 350 - tx || x<= tx) dsx = - dsx;
+        if ( y >= 450 - ty || y<=ty ) dsy = - dsy;
+        }
+    
+    public void pinta (Graphics g) {
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.drawImage(this.image, x, y, null);
+        }
+    
+
+    public void run() {
+        while (true) { 
+            //System.out.println("Movent nau numero " + this.nomNau);
+            try { Thread.sleep(this.v); } catch (Exception e) {}
+            moure();
+            }
+        }
+    
+    public void esquerra() {
+        this.dsx = -10; 
+        }
+    
+    public void dreta() {
+        this.dsx = 10; 
+        }
+    public void parar() {
+        this.dsx = 0; 
+        }
+    public int getY() {
+    	return this.y;
+    }
+    public int getX() {
+    	return this.x;
+    }
+    
+    
+    }
